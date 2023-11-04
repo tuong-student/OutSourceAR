d6b407246168d1b9839aa03c57045ea4294ee573
NOOD_NTT <thanhtuongnguyen1@gmail.com>
2023-11-04 20:44:05 +0700

    After clean up

--- Assets/Scripts/Game/UI/UIMain/Inventory.cs
+++ Assets/Scripts/Game/UI/UIMain/Inventory.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+namespace Game
 {
-    [SerializeField] private List<InventoryElement> _inventoryElements = new List<InventoryElement>();
-    [SerializeField] private List<ARObjectSO> _arObjectData = new List<ARObjectSO>();
-
-    void Awake()
+    public class Inventory : MonoBehaviour
     {
-        for(int i = 0; i < _arObjectData.Count - 1; i++) 
+        [SerializeField] private List<InventoryElement> _inventoryElements = new List<InventoryElement>();
+        [SerializeField] private List<ARObjectSO> _arObjectData = new List<ARObjectSO>();
+
+        void Awake()
         {
-            ARObjectSO data = _arObjectData[i];
-            _inventoryElements[i].SetIcon(data._iconSprite);
+            for(int i = 0; i < _arObjectData.Count - 1; i++) 
+            {
+                ARObjectSO data = _arObjectData[i];
+                _inventoryElements[i].SetIcon(data._iconSprite);
+            }
         }
     }
 }
